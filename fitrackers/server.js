5dc0a5c999ef7760ece32d9c0fa80395e9354f6d
Pamela Holmes <phfullsail@fullsail.edu>
2015-05-31 07:49:27 -0500

    redoing the structure

--- fitrackers/server.js
+++
@@ -1,6 +0,0 @@
-// config/database.js
-/*module.exports = {
-
-    'url' : 'https://my.modulus.io/database/sapesI8b/users' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
-
-};*/
